d44183b67d0c08c23ca3ddc00f3c508241f89515
vdtmn8 <54950512+vdtmn8@users.noreply.github.com>
2022-09-02 16:56:32 -0400

    math for monitors

--- rotate.py
+++ rotate.py
@@ -1,43 +1,47 @@
-from turtle import color, width
+import math
 import matplotlib
 from matplotlib import animation
 from matplotlib.collections import PatchCollection
 from matplotlib.patches import Rectangle
 import matplotlib.pyplot as plt
 from matplotlib.transforms import Affine2D
-
 import numpy as np
 
-### Monitor Stats
+### Monitor 
 mon1_dem = (16, 9)
-mon1_pos = (0, 0)
-
 mon2_dem = (16, 9)
-mon2_pos = (mon1_pos[0] + mon1_dem[0], mon1_pos[1])
 
-rot_around1 = (0 - mon1_dem[0]/2, 0)
-rot_around2 = (0 + mon2_dem[0], 0)
+center_to_corner1 = math.sqrt(mon1_dem[0]**2 + mon1_dem[1]**2) / 2
+center_to_corner2 = math.sqrt(mon2_dem[0]**2 + mon2_dem[1]**2) / 2
 
-# Center = [-8, 0]
-point_of_rotation1 = np.array([0 - mon1_dem[0], 0 -mon1_dem[1]/2])
-# Center = [8, 0]
-point_of_rotation2 = np.array([mon2_dem[0]/2 - mon1_dem[0], mon2_dem[1]/2])
+horizontal_rad1 = mon1_dem[0]/2
+horizontal_rad2 = mon2_dem[0]/2
 
 
-color1 = "#02a9f7"
-color2 = "#89d6fb"
+rot_around1 = [0 - mon1_dem[0]/2, 0]
+rot_around2 = [0 + mon2_dem[0]/2, 0]
+
+
+
+# Center = [-8, 0], Bottom Left = [-16, -4.5]
+point_of_rotation1 = np.array([0 - mon1_dem[0], 0 - mon1_dem[1]/2])
+# Center = [8, 0], Bottom Left = [0, -4.5]
+point_of_rotation2 = np.array([0, 0 - mon2_dem[1]/2])
 
 ### Set Up
 fig, ax = plt.subplots()
-patches = []
 
-rot = 361
+# Aesthetic
+color1 = "#02a9f7"
+color2 = "#89d6fb"
 
+rot = 91
 for deg in range(0, rot):
+    # Clear for Animation
     plt.cla()
 
     rec1 = Rectangle(point_of_rotation1, mon1_dem[0], mon1_dem[1], color="y", transform=Affine2D().rotate_deg_around(*(rot_around1[0], rot_around1[1]), deg)+ax.transData)
-    rec2 = Rectangle(-point_of_rotation2, mon2_dem[0], mon2_dem[1], color="b", transform=Affine2D().rotate_deg_around(*(rot_around2[0],rot_around2[1]), deg)+ax.transData)
+    rec2 = Rectangle(point_of_rotation2, mon2_dem[0], mon2_dem[1], color="b", transform=Affine2D().rotate_deg_around(*(rot_around2[0],rot_around2[1]), deg)+ax.transData)
 
     ax.add_patch(rec1)
     ax.add_patch(rec2)
@@ -46,11 +50,7 @@ for deg in range(0, rot):
     plt.plot([rot_around2[0]], [rot_around2[1]], marker="o")
 
     ### Plot
-    # plt.xlim(-3, 35)
-    # plt.ylim(-12, 21)
-
     plt.axis("scaled")
-    # plt.axis("off")
     plt.pause(0.0001)
 
 plt.show()
